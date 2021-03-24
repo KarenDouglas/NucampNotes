9082dfd6b6f032e3a673e7ffac3fdb2afd274a88
KarenDouglas <79128405+KarenDouglas@users.noreply.github.com>
2021-03-24 10:47:58 -0500

    jquery styles w4 w5

---
+++ js/Week4-5.js
@@ -0,0 +1,119 @@
+$(function(){
+//     $("#sectionHeader1").mouseenter(function() {
+//         $("#sectionHeader1").css("background-color"," red");
+//       })
+//    $("#sectionHeader1").click(function() {
+//         $("#sectionBody1").slideToggle();
+//       })
+
+      $("#sectionHeader1").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody1").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader2").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody2").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader3").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody3").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader4").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody4").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader5").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody5").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader6").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody6").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader7").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody7").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader8").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody8").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader9").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody9").slideToggle("slow");
+        }
+      });
+      $("#sectionHeader10").on({
+        mouseenter: function(){
+          $(this).css( "background-color", "lightgray");
+        },
+        mouseleave: function(){
+          $(this).css("background-color", "grey");
+        },
+        click: function(){
+          $("#sectionBody10").slideToggle("slow");
+        }
+      });
+  });
